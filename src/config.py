c39ea4657014920dde1bf74034c8044fe1a5f472
Rahul Arora <arora.rahul59@gmail.com>
2026-03-25 10:51:18 +0530

    add: config

---
+++ src/config.py
@@ -0,0 +1,9 @@
+PDF_PATH = "/Users/apple/Desktop/stuff/Projects/production-rag-pipeline/data/the fear bubble ch-1.pdf"
+
+CHUNK_SIZE = 500
+CHUNK_OVERLAP = 50
+
+EMBEDDING_MODEL = "text-embedding-3-small"
+
+LLM_MODEL = "gpt-3.5-turbo"
+CHAIN_TYPE = "stuff"
